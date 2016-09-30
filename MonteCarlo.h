161da94f47a4fa0a2052407e2231c95c16ca06e5
shawn1776 <zshawn1776@gmail.com>
2016-09-30 17:44:57 -0400

    add Vlj

--- MonteCarlo.h
+++ MonteCarlo.h
@@ -7,8 +7,8 @@
 #include <limits.h>
 
 // for Fene; maximum deviation is r-ro = R
-#define K 98.0/5.0 						// spring constant, in(energy/distance^2)
-#define R (3.0/7.0)  						// (distance)
+#define K (98.0/5.0) 					// spring constant, in(energy/distance^2)
+#define R (3.0/7.0)  					//  be careful of the BRACKE!!!  // (distance)
 #define r0 1.0     						// equilibrium bond length (distance)
 #define R_Fcutoff_l 0.571428571428572   // the left boundary of the FENE potential
 #define R_Fcutoff_r 1.428571428571428   // FENE right boundary  
@@ -16,9 +16,9 @@
 
 // constant for LJ potential
 #define EPSILON 1.0                     // epsilon (energy)
-#define SIGMA 1.0/(pow(2,1.0/6.0))      // sigma (distance)    1.0/1.12246205 = 0.832490
+#define SIGMA (1.0/(pow(2,1.0/6.0)))    // sigma (distance)    (1.0/1.122462048309373 = 0.8908987181403393)
 #define epsilon_LJ 1.0
-#define V_LJ_cut -0.016316891136        // energy cutoff for LJ potential
+#define V_LJ_cut -0.016316891136        // energy cutoff for LJ potential, accurate to the 12th digit,,,
 #define R_LjCutoff_l 0.1                // LJ left boundary, it will return a huge U_Lj energy number, but it is toke care by the energy boundray dE/T < 600
 #define R_LjCutoff_r 2.22724679535085   // LJ right boundary, a cutting off distance which we choose to make the Lennard-Jones potential go to 0 when the distance between two beads is larger than r_c
 
@@ -26,74 +26,72 @@
 using namespace std;
 
 double calcdist(double a, double b, double c, double x, double y, double z);
-
 // /** calc_dist_v
 //   * 
 //   */
 // std::vector<double> calc_dist_v();
-
-double sumVf( int n, std::vector<double> &v);
-double sumVlj(int n, std::vector<double> &v);// input the a[][] Or b[][]
-
+double  sumVf(int size, std::vector<double> &v);
+double sumVlj(int size, std::vector<double> &v);// input the a[][] Or b[][]
 
 
-
- class MonteCarlo{
-
-
- public:
+class MonteCarlo{
+ 	public:
  	// Metroplis update a single monomer of a polymer
 	double Metroplis_single_monomer(vector<double> &v, double T, int seed);
+
 
  };
 
 
- 
-double MonteCarlo::Metroplis_single_monomer(vector<double> &v, double T, int seed){
+// Metroplis_single_monomer
+ // precondition: 1) structure-> calculate the totalE = Vlj+Vfene
+ //				  2) temperature
+//				  3) random number
+double MonteCarlo::Metroplis_single_monomer(vector<double> &v, double T, double random){
+	double V_lj=0.0, V_fene = 0.0, Eold =0.0, Enew =0.0;
 
+	cout <<"V_lj: "<<V_lj <<endl;
 
+	return 0.0;
 }
-
-
-
-
-// 
+ 
 double calcdist( double a, double b, double c, double x, double y, double z){	
    return sqrt(pow(a - x, 2) + pow(b - y, 2) + pow(c - z, 2));
 }
 
 // calclate the sum of Vfene on given the coord vector
-double sumVf( int n, std::vector<double> &v){
+double sumVf(int size, std::vector<double> &v){
 	double Vfene = 0.0;
-	int size = (v.size()+1)/3;
-	cout<<"size is "<<size<<endl;
 	double r = 0.0; 
-	double coefficient = -0.5*K*R*R;
-	
-	for (int i=0; i<1; i++){
+	double coefficient = -0.5*K*R*R;		// coefficient = 1/2.0 *K*R^2
+	for (int i=0; i<(size-1); i++){
 		r = calcdist(v[i],v[i+size],v[i+size+size],v[i+1],v[i+1+size],v[i+1+size+size]);
-		//cout <<"r "<<r<<endl;
-		// r is in between (R_Fcutoff_l and R_Fcutoff_r)
 		if(r <= R_Fcutoff_l || r >= R_Fcutoff_r){
 			return DBL_MAX;
 		}else{
-			Vfene = 0.0;
-			r=1.2;
-			cout<<"r is "<<r<<endl;
-			cout<<"coeff is "<<coefficient<<endl;
-			cout<< "r0 "<<r0<<endl;
-			cout<< "R "<<R<<endl;
-
-			cout <<"(r-r0): "<<(r-r0) <<" R: "<<R<<" (r-r0)/R: "<< (r-r0)/R<<endl;
-			Vfene = Vfene + coefficient*log(1.0-(r-r0)*(r-r0)/R/R);
-			cout <<"vf: "<<coefficient*log(1.0-(r-r0)*(r-r0)/R/R)<<endl;
+			Vfene += coefficient*log(1.0-(r-r0)*(r-r0)/R/R);
+			// cout<<i<<" th distance: "<<setw(15)<< setprecision(10)<<Vfene<<" r is "<<r<<endl; // debug
 		}//if elif
 	}//for 
+		cout <<"sigma: "<<SIGMA<<endl;
 		return Vfene;	
 }
 
 
 // calclate the sum of V_Lj on given the coord vector
-double sumVlj(int n, std::vector<double> &v){
-	return 0.0;
+double sumVlj(int size, std::vector<double> &v){
+	double VLj =0.0,r=0.0;
+	int cnt =0;  // i,j when j is not i+1 or i-1
+	for (int i = 0; i<size-2; i++){
+		for(int j = i+2; j<size; j++){
+			cnt++;
+			r = calcdist(v[i],v[i+size],v[i+size+size],v[j],v[j+size],v[j+size+size]);
+			if(r <= R_LjCutoff_l){ // if r is smaller than left boundary, then, return max energy
+				return DBL_MAX;
+			}else if(r <= R_LjCutoff_r){ // if r exceeds the right boundary then, return 0 energy
+				VLj += 4.0*EPSILON*(pow((SIGMA/r),12.0)-pow((SIGMA/r),6.0))-V_LJ_cut;
+			} // if -else if
+		}//j
+	}//i
+	return VLj;
 }
--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ int main(){
 	ploymer p1(13);
 	p1.outputCoordv();
 	MonteCarlo mc1;
+
 	typedef vector<double> vd;
-	vd vv(39,0);
+	vd vv(40,0);
 
 	for(int i=0; i<vv.size()/3; i++ ){
 		vv[i] = i*1.0;
@@ -17,7 +18,13 @@ int main(){
 	}
 	vv[1]=1.2;
 	cout<<vv[1]<<endl;
-	double Vf = sumVf(39,vv); 
-	cout<<"vf:  "<<Vf<<endl;
+	int size = floor((vv.size()+1)/3);
+	cout<<"size is "<<size<<endl;
+
+	double Vf = sumVf(size,vv); 
+	cout<<"vf:  "<<setw(15)<< setprecision(10)<<Vf<<endl;
+	sumVlj(6,vv);
+	double V_lj=0.0, V_fene = 1.0;
+	cout <<"V_lj: "<<V_lj <<endl;
 	return 0;
 }
