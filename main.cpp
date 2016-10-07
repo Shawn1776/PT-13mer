4dd5beed6389624802a2bb85329c0b1b346b1d21
shawn1776 <zshawn1776@gmail.com>
2016-10-07 16:33:00 -0400

    single core testing; need to add histogram festure in MC class maybe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "polymer.h"
 #include "ranMARS.h"
 #include "MonteCarlo.h"
+#include <fstream>
 
 
 using namespace std;
@@ -17,12 +18,29 @@ int main(){
 	int size = floor((p1.coordv.size()+1)/3);
 	double temperature = 1.0;
 	// Mento Carlo Updates, each mono-mer
-	int totalMC = 13*1000000; // 100 MC swap
-	for (int t =0; t<totalMC; t++){
-		if ((t+1)%13==0){
-			cout << (t+1)/13 <<"	"<<mc1.Metropolis_single_monomer(size,p1.coordv,temperature)<<endl;
+	int totalMC = 13*100000; //E5 *000; // 100 MC swap
+	string filename ="E_";
+	for(int tn =0; tn<4; tn++){ // tn-> temperature number
+		double temperature = 1.0;
+		temperature += tn*0.1;
+		//string filename= static_cast<ostringstream*>( &(ostringstream() << temperature) )->str();
+		string filename ="E_";
+		filename.append(to_string(temperature));
+		ofstream outfile;
+		outfile.open(filename, ios::out);
+		
+		for (int t =0; t<totalMC; t++){
+			double E = mc1.Metropolis_single_monomer(size,p1.coordv,temperature); // efficince, need to pass the calculated E back to the function
+			if ((t+1)%1300==0){
+				//cout << (t+1)/1300 <<"	"<< E <<endl; // output every 1300, 100 swap
+				outfile <<(t+1)/1300 <<"	"<< E <<endl;
+			}
+			
 		}
+		outfile.close();
+
 	}
+	
 	//p1.outputCoordv();
 	return 0;
 }
